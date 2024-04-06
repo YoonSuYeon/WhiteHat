e2a90dd2b3960e215b25ee2563786a10aa364e43
Yoon Suyeon <138573507+YoonSuYeon@users.noreply.github.com>
2024-04-07 02:15:14 +0900

    Add files via upload
    3rd code | can see result

--- network_security/sniffed_improved.c
+++ network_security/sniffed_improved.c
@@ -2,7 +2,8 @@
 #include <stdio.h>
 #include <pcap.h>
 #include <arpa/inet.h>
-#include "myheader.h"
+#include <netinet/ether.h>
+// #include "my_fixedheader.h"
 
 /* Ethernet Header */
 struct ethheader {
@@ -55,12 +56,12 @@ void got_packet(u_char *args, const struct pcap_pkthdr *header,
   struct ethheader *eth = (struct ethheader *)packet;
 
   if (ntohs(eth->ether_type) == 0x0800) { // 0x0800 is IP type
-    struct ipheader * ip = (struct ipheader *)
-                           (packet + sizeof(struct ethheader)); 
-   struct tcpheader *tcp = (struct tcpheader *)(packet + sizeof(struct ethheader) + ip->iph_ihl * 4);
+    struct ipheader * ip = (struct ipheader *)(packet + sizeof(struct ethheader)); 
+    struct tcpheader *tcp = (struct tcpheader *)(packet + sizeof(struct ethheader) + ip->iph_ihl * 4);
+	
 	// printf("       From: %s\n", inet_ntoa(ip->iph_sourceip));   
 	// printf("         To: %s\n", inet_ntoa(ip->iph_destip));    
-	 // Ethernet 정보 출력
+	// Ethernet 정보 출력
 	 for (int i = 0; i < 6; i++) {
         eth->ether_shost[i] = i + 1; // 예시 MAC 주소 설정
         eth->ether_dhost[i] = 6 - i; // 예시 MAC 주소 설정
@@ -92,21 +93,37 @@ void got_packet(u_char *args, const struct pcap_pkthdr *header,
     // TCP 포트 정보 출력
     printf("Source Port: %d\n", ntohs(tcp->tcp_sport));
     printf("Destination Port: %d\n", ntohs(tcp->tcp_dport));
-    /* determine protocol */
-    switch(ip->iph_protocol) {                                 
-        case IPPROTO_TCP:
-            printf("   Protocol: TCP\n");
-            return;
-        case IPPROTO_UDP:
-            printf("   Protocol: UDP\n");
-            return;
-        case IPPROTO_ICMP:
-            printf("   Protocol: ICMP\n");
-            return;
-        default:
-            printf("   Protocol: others\n");
-            return;
+    // printf("\n");
+
+
+    // TCP 헤더 길이 계산
+    int tcp_header_len = tcp->tcp_offx2 >> 4; // TCP 헤더 길이는 4바이트 단위로 표현되므로 비트 이동을 통해 계산합니다.
+
+    // 데이터 영역 출력 (메시지 출력)
+    int data_len = ntohs(ip->iph_len) - (ip->iph_ihl * 4) - (tcp_header_len * 4);
+    if (data_len > 0) {
+        printf("Message: ");
+        for (int i = 0; i < data_len && i < 10; i++) { // 메시지의 처음 10바이트만 출력
+            printf("%c", packet[sizeof(struct ethheader) + ip->iph_ihl * 4 + tcp_header_len * 4 + i]);
+        }
+        printf("\n");
     }
+    printf("\n");
+    // /* determine protocol */
+    // switch(ip->iph_protocol) {                                 
+    //     case IPPROTO_TCP:
+    //         printf("   Protocol: TCP\n");
+    //         return;
+    //     case IPPROTO_UDP:
+    //         printf("   Protocol: UDP\n");
+    //         return;
+    //     case IPPROTO_ICMP:
+    //         printf("   Protocol: ICMP\n");
+    //         return;
+    //     default:
+    //         printf("   Protocol: others\n");
+    //         return;
+    // }
   }
 }
 
@@ -115,24 +132,26 @@ int main()
   pcap_t *handle;
   char errbuf[PCAP_ERRBUF_SIZE];
   struct bpf_program fp;
-  char filter_exp[] = "icmp";
+  // char filter_exp[] = "icmp";
   bpf_u_int32 net;
 
   // Step 1: Open live pcap session on NIC with name enp0s3
   handle = pcap_open_live("enp0s1", BUFSIZ, 1, 1000, errbuf);
 
   // Step 2: Compile filter_exp into BPF psuedo-code
-  pcap_compile(handle, &fp, filter_exp, 0, net);
-  if (pcap_setfilter(handle, &fp) !=0) {
-      pcap_perror(handle, "Error:");
-      exit(EXIT_FAILURE);
-  }
+  // pcap_compile(handle, &fp, filter_exp, 0, net);
+  // if (pcap_setfilter(handle, &fp) !=0) {
+  //     pcap_perror(handle, "Error:");
+  //     exit(EXIT_FAILURE);
+  // }
 
   // Step 3: Capture packets
-  pcap_loop(handle, -1, got_packet, NULL);
+  // pcap_loop(handle, -1, got_packet, NULL);
+  pcap_loop(handle, 0, got_packet, NULL);
 
   pcap_close(handle);   //Close the handle
   return 0;
 }
+
 
 
